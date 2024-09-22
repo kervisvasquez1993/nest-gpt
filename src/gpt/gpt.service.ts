dc48a86ea13f52a092effed986a9396b05863ada
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-09-22 13:14:19 -0300

    update date

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateGptDto } from './dto/create-gpt.dto';
 import { OrtographiUseCase } from './use-cases/ortography.use-case';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
+  private openai = new OpenAI({
+    apiKey: process.env.API_GPT2,
+    organization: process.env.ORGANIZATION,
+    project: process.env.PROJECT,
+  });
+
   async orthography(CreateGptDto: CreateGptDto) {
-    return await OrtographiUseCase({
-      prompt: CreateGptDto.prompt,
-    });
+    try {
+      const response = await OrtographiUseCase(this.openai, {
+        prompt: CreateGptDto.prompt,
+      });
+      return response;
+    } catch (error) {
+      console.error('Error in orthography service:', error.message);
+      throw new Error('Failed to process request. Please try again later.');
+    }
   }
 }
--- src/gpt/use-cases/ortography.use-case.ts
+++ src/gpt/use-cases/ortography.use-case.ts
@@ -1,9 +1,41 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
-export const OrtographiUseCase = async (option: Options) => {
+export const OrtographiUseCase = async (openai: OpenAI, option: Options) => {
   const { prompt } = option;
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-4',
+    temperature: 0.3,
+    max_tokens: 300,
+    messages: [
+      {
+        role: 'system',
+        content: `
+        Eres una herramienta que propone ideas de startups basadas en los inputs de los usuarios.
+        Tu misión es generar ideas innovadoras, tecnológicas y creativas, teniendo en cuenta el sector de interés, nivel de experiencia y los recursos mencionados por el usuario.
+        
+        Además, para cada idea propuesta, debes proporcionar una documentación completa y un manual de uso. La documentación debe incluir:
+        1. Descripción de la idea de la startup.
+        2. Estructura técnica necesaria para implementar la idea.
+        3. Potenciales modelos de negocio.
+        4. Etapas detalladas para desarrollar la idea, desde la fase inicial hasta el lanzamiento.
+        5. Tecnologías recomendadas y frameworks adecuados.
+
+        El manual de uso del proyecto debe explicar cómo poner en práctica cada etapa de manera accesible para cualquier nivel de experiencia, con sugerencias de herramientas, plataformas y recursos. 
+        Ademas de todo eso debes responder en un formato JSON.
+      `,
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+  });
+
   return {
     prompt: prompt,
+    completion: completion,
   };
 };
